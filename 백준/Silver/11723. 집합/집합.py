f1ef57a469f051748ba7addabaf69b35414cefb0
이예지 <156265319+Sophia0705@users.noreply.github.com>
2025-01-19 23:12:59 +0900

    [Silver V] Title: 집합, Time: 3488 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,34 +1,24 @@
 import sys
 input = sys.stdin.readline
 
-def solve(do, x=None):
-    global S
-    if do == 'add' and x not in S:
-        S.add(x)
-    elif do == 'remove' and x in S:
-        S.remove(x)
-    elif do == 'check':
-        if x in S:
-            print(1)
-        else:
-            print(0)
-    elif do == 'toggle':
-        if x in S:
-            S.remove(x)
-        else:
-            S.add(x)
-    elif do == 'all':
-        S = set(str(i) for i in range(1, 21))
-    elif do == 'empty':
-        S.clear()
-
 m = int(input())
-S = set()
+S = 0
 for _ in range(m):
     command = input().rstrip().split()
-    do = command.pop(0)
-    if command:
-        x = command[0]
-        solve(do, x)
-    else:
-        solve(do)
+    op = command[0]
+
+    if op == 'all':
+        S = (1 << 20) -1    # 20개의 비트를 모두 1로 설정
+    elif op == 'empty':
+        S = 0
+    else:   # x 있을 때
+        x = int(command[1]) -1
+
+        if op == 'add':
+            S |= (1 << x)
+        elif op == 'remove':
+            S &= ~(1 << x)
+        elif op == 'check':
+            print(1 if S & (1 << x) else 0)
+        elif op == 'toggle':
+            S ^= (1 << x)
