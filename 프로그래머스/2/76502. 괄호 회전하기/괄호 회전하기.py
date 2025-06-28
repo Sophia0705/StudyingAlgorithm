0f5d070871df70e9c519204f728a01d6cb817c85
이예지 <156265319+Sophia0705@users.noreply.github.com>
2025-06-29 01:17:06 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,21 +1,3 @@
-def check(s):
-    stack = []
-    for ch in s:
-        if ch in '([{':
-            stack.append(ch)
-        else:
-            if not stack:
-                return False
-            top = stack.pop()
-            if (top == '(' and ch != ')') or (top == '{' and ch != '}') or (top == '[' and ch != ']'):
-                return False
-    return not stack
-
 def solution(s):
-    answer = 0
-    n = len(s)
-    for i in range(n):
-        rotated = s[i:] + s[:i]
-        if check(rotated):
-            answer += 1
+    answer = -1
     return answer
