21f5a3af62ee34c2faa048a9a49afe14f5cb812f
이예지 <156265319+Sophia0705@users.noreply.github.com>
2025-07-12 11:27:45 +0900

    [Silver I] Title: 경로 찾기, Time: 140 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -1,23 +1,23 @@
-# 11403. 경로 찾기 in DFS(stack)
+# 11403. 경로 찾기
+# 모든 정점(i, j)에서 i -> j 가는 길이가 양수인 경로 있는지(1)? 없는지(0)?
 
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
-ans = []
+N = int(input())    # 정점의 개수
+graph = [list(map(int, input().split())) for _ in range(N)] # 인접 행렬
+answer = [[0]*N for _ in range(N)]
 
-for i in range(n):
-    visited = [False] * n
-    stack = [i]
-
-    while stack:
-        cur = stack.pop()
-        for next in range(n):
-            if arr[cur][next] == 1 and not visited[next]:
+for i in range(N):
+    visited = [False] * N
+    st = [i]
+    while st:
+        current = st.pop()
+        for next in range(N):
+            if graph[current][next] == 1 and not visited[next]:
                 visited[next] = True
-                stack.append(next)
-    ans.append([1 if visited[j] else 0 for j in range(n)])
+                st.append(next)
+                answer[i][next] = 1
 
-for r in ans:
+for r in answer:
     print(*r)
