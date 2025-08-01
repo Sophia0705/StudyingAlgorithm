93b8eb034bc6d630d35d1f9ca9d6e04cf86eeb3f
이예지 <156265319+Sophia0705@users.noreply.github.com>
2025-08-02 01:07:41 +0900

    [Silver I] Title: IOIOI, Time: 324 ms, Memory: 34368 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,17 +1,23 @@
-# 1389. 케빈 베이컨의 6단계 법칙
-import sys
-input = sys.stdin.readline
+n = int(input())
+m = int(input())
+s = input()
 
-N = int(input())
-M = int(input())
-S = input().strip()
-ans = 0
-Pn = 'I'+('OI')*N
-Plen = len(Pn)
+count = 0
+i = 0
 
-for i in range(M):
-    # S에서 N 길이만큼 비교하면서 Pn이랑 일치하면 ans += 1
-    if S[i:i+Plen] == Pn:
-        ans += 1
+while i < m - 1:
+    # "IO"로 시작하는 부분을 찾음
+    if s[i:i+2] == "IO":
+        pattern_count = 0
+        # 연속된 "IOI" 패턴의 개수를 셈
+        while i < m - 2 and s[i:i+3] == "IOI":
+            pattern_count += 1
+            i += 2  # "IOI"에서 다음 "IOI"로 이동 (2칸씩)
+        
+        # P_N 패턴이 몇 개 들어있는지 계산
+        if pattern_count >= n:
+            count += pattern_count - n + 1
+    
+    i += 1
 
-print(ans)
+print(count)
