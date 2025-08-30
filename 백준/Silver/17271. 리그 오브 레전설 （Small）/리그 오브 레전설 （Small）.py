6a7a78d3bcfeb8123b40c8a666624d82be0c8786
이예지 <156265319+Sophia0705@users.noreply.github.com>
2025-08-30 23:53:36 +0900

    [Silver II] Title: 리그 오브 레전설 (Small), Time: 36 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/17271. 리그 오브 레전설 （Small）/리그 오브 레전설 （Small）.py
+++ 백준/Silver/17271. 리그 오브 레전설 （Small）/리그 오브 레전설 （Small）.py
@@ -4,8 +4,9 @@ input = sys.stdin.readline
 
 N, M = map(int, input().split())    # N: 싸움시간, B스킬: M초 (A스킬: 1초)
 dp = [1] * (N+1)
+mod = 1000000007
 
 for i in range(M, N+1): # M초 보다 작으면 어차피 B스킬 못씀(A만 가능)
-    dp[i] = (dp[i-1] + dp[i-M])
+    dp[i] = (dp[i-1] + dp[i-M]) % mod
 
-print(dp[N] % 1000000007)
+print(dp[N])
