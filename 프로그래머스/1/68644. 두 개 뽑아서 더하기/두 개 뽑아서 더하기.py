279edd1779625ad4e2c09fe51932b16bbf95408b
이예지 <156265319+Sophia0705@users.noreply.github.com>
2025-09-26 16:52:14 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.98 ms, Memory: 9.43 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -1,10 +1,9 @@
+from itertools import permutations
 def solution(numbers):
-    answer = []
-    sumV = 0
-    for i in range(len(numbers)):
-        for j in range(i+1, len(numbers)):
-            sumV = numbers[i] + numbers[j]
-            if sumV not in answer:
-                answer.append(sumV)
-    answer.sort()
-    return answer
+    temp = set()
+    perms = set(permutations(numbers, 2))
+    for p in perms:
+        temp.add(sum(p))
+    listed_ans = sorted(list(temp))
+    
+    return listed_ans
