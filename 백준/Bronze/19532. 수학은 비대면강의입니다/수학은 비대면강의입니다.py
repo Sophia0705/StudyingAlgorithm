97972e19a6be9d6063c8049967a9c432093ea87b
이예지 <156265319+Sophia0705@users.noreply.github.com>
2025-09-29 03:05:39 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 788 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -0,0 +1,12 @@
+# 19532. 수학은 비대면강의입니다
+import sys
+input = sys.stdin.readline
+
+a, b, c, d, e, f = map(int, input().split())
+ans = []
+for x in range(-999, 1000):
+    for y in range(-999, 1000):
+        if (a*x) + (b*y) == c and (d*x) + (e*y) == f:
+            ans.append(x)
+            ans.append(y)
+print(*ans)
