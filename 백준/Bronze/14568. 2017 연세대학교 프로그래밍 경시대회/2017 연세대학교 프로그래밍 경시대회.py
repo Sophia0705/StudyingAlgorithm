cfa71a69ac5923b307b99a5747edaab11abc4e3b
이예지 <156265319+Sophia0705@users.noreply.github.com>
2025-09-29 01:03:03 +0900

    [Bronze III] Title: 2017 연세대학교 프로그래밍 경시대회, Time: 136 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.py
+++ 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.py
@@ -5,13 +5,10 @@ from itertools import product
 
 # a, b, c : 택희, 영훈, 남규 -> (a, b, c) 수
 n = int(input())    # 사탕의 총 개수
-possible = [i for i in range(1, n)]
 ans = 0
-pros = product(possible, repeat=3)
-for pro in pros:
-    if sum(pro) != n:
-        continue
-    a, b, c = pro[0], pro[1], pro[2]
-    if c >= b + 2 and a % 2 == 0:
-        ans += 1
+for a in range(1, n):   # 택희
+    for b in range(1, n):   # 영훈
+        for c in range(1, n):   # 남규
+            if a + b + c == n and c >= b+2 and a % 2 == 0:
+                ans += 1
 print(ans)
